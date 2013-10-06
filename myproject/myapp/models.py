e5b5ba4047cc56d6df45d1ec0de0f9b12f71a656
Harry <hjwp2@cantab.net>
2013-10-06 18:56:34 +0100

    basic project with 2 models

---
+++ myproject/myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class List(models.Model):
+    pass
+
+class Item(models.Model):
+    list = models.ForeignKey(List)
+    text = models.TextField()
